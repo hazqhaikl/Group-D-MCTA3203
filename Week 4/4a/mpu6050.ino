100f3150d5c4f2e9fc1238b5dd9d82a0cfce353e
hazqhaikl <142820253+hazqhaikl@users.noreply.github.com>
2023-11-08 11:36:52 +0800

    Update mpu6050.ino

--- Week 4/4a/mpu6050.ino
+++ Week 4/4a/mpu6050.ino
@@ -1,1 +1,27 @@
+#include <Wire.h>
+#include <MPU6050.h>
 
+MPU6050 mpu;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  mpu.initialize();
+}
+void loop() {
+  int ax, ay, az, gx, gy, gz;
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  Serial.print("Accel: ");
+  Serial.print(ax);
+  Serial.print(", ");
+  Serial.print(ay);
+  Serial.print(", ");
+  Serial.print(az);
+  Serial.print(" Gyro: ");
+  Serial.print(gx);
+  Serial.print(", ");
+  Serial.print(gy);
+  Serial.print(", ");
+  Serial.println(gz);
+  delay(300);
+}
